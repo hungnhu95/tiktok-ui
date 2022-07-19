668606d53f399ca418ca4690349ebc780b33b0cc
hungnhu95 <ngaydeptuoi45@gmail.com>
2022-07-19 14:54:00 +0700

    update prettierrc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { publicRoutes } from '~/routes'
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-          <Routes>
-            {publicRoutes.map((route, index) => {
-              const Page = route.component
-              return <Route key={index} path={route.path} element={<Page />} />
-            })}
-          </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import Home from '~/pages/Home'
 import Following from '~/pages/Following'
 import Profiles from '~/pages/Profiles'
+import Upload from '~/pages/Upload'
+
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profiles', component: Profiles },
+    { path: '/upload', component: Upload, layout: null },
 
 ]
 
